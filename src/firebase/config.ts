819cef22c465a34f609ed07c1144d6c86746a526
Lalit Mistry <appjxn@gmail.com>
2026-01-03 16:13:09 +0000

    Try fixing this error: `Console FirebaseError: Firebase: Error (auth/api

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,12 +1,11 @@
-
 // This configuration is public and can be safely used in the client-side.
 // It's sourced from the Firebase Console.
 export const firebaseConfig = {
   "projectId": "bizcentral1-31429890-418b0",
   "appId": "1:843423687874:web:863764f1260dd8cd01c75b",
-  "storageBucket": "bizcentral1-31429890-418b0.firebasestorage.app",
-  "apiKey": "AIzaSyBNczj3XQGTX7y2L5jsXcrr2U1uZUQSrdE",
+  "storageBucket": "bizcentral1-31429890-418b0.appspot.com",
+  "apiKey": "AIzaSyACRiDv9BMFT6vCIqI2uciMLSyGbAlu76M",
   "authDomain": "bizcentral1-31429890-418b0.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "843423687874"
+  "messagingSenderId": "843423687874",
+  "measurementId": "G-L5S59TC6B5"
 };
